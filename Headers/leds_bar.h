a003715d9f3704f52ad284ed4d476ad127c9ea27
Cássio Santos <sss.cassio@gmail.com>
2018-11-15 14:58:06 +0000

    Change project structure

---
+++ Headers/leds_bar.h
@@ -0,0 +1,15 @@
+/*
+ * leds_bar.h
+ *
+ *  Created on: 8 de nov de 2018
+ *      Author: ssscassio
+ */
+
+#ifndef HEADERS_LEDS_BAR_H_
+#define HEADERS_LEDS_BAR_H_
+
+void leds_bar_init(void);
+void leds_bar_output(int number_of_leds);
+
+
+#endif /* HEADERS_LEDS_BAR_H_ */
--- Source/leds_bar.c
+++ Source/leds_bar.c
@@ -1,11 +1,16 @@
+/*
+ * led_array.c
+ *
+ *  Created on: 8 de nov de 2018
+ *      Author: ssscassio
+ */
 #include <avr/io.h>
-#include <util/delay.h>
-#include "../Headers/leds_bar.h"
 
 #define LEDS_BAR_PORT PORTC /* Define LEDs bar at port C */
 #define DS_PIN PC0          /* Serial data input for shift register at pin PC0 */
 #define ST_CP_PIN PC1       /* Latch clock at pin PC1 (ST_CP) */
 #define SH_CP_PIN PC2       /* Shift clock at pin PC2 (SH_CP) */
+
 
 // Define Shift Register actions
 #define DS_low()  LEDS_BAR_PORT&=~(1<<DS_PIN)
@@ -15,23 +20,24 @@
 #define SH_CP_low()  LEDS_BAR_PORT&=~(1<<SH_CP_PIN)
 #define SH_CP_high() LEDS_BAR_PORT|=(1<<SH_CP_PIN)
 
-//Define functions
-//======================
-void hardware_init(void);
-
-int main (void) {
-  hardware_init(); //Setup IO pins and defaults
-
-  while(1) {
-    for(int i=0; i<9; i++){
-      leds_bar_output(i);
-    }
-  }
+void leds_bar_init() {
+  /* Define Shift Register pins as Outputs - remaining pins unconfigured */
+  DDRC  |= (1<<DS_PIN)
+    | (1<<ST_CP_PIN)
+    | (1<<SH_CP_PIN);
 }
 
-
-void hardware_init (void) {
-
-  leds_bar_init();
-  LEDS_BAR_PORT = 0x00; // Set all output pins as low
+void leds_bar_output(int number_of_leds) {
+  SH_CP_low();
+  ST_CP_low();
+  for (int i=0;i<8; i++) {
+      if (i < number_of_leds) {
+        DS_high();
+      } else{
+        DS_low();
+      }
+      SH_CP_high();
+      SH_CP_low();
+  }
+  ST_CP_high();
 }
