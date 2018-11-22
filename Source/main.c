6d979103b7e6ddc9301f1739cc286e30cedc8fc0
Cássio Santos <sss.cassio@gmail.com>
2018-11-22 15:54:09 +0000

    Fix main file

--- Source/main.c
+++ Source/main.c
@@ -1,19 +1,9 @@
 #include <avr/io.h>
 #include <util/delay.h>
-#include "../Headers/leds_bar.h"
+#include "../Headers/shift_register.h"
+#include "../Headers/uart.h"
 
 #define LEDS_BAR_PORT PORTC /* Define LEDs bar at port C */
-#define DS_PIN PC0          /* Serial data input for shift register at pin PC0 */
-#define ST_CP_PIN PC1       /* Latch clock at pin PC1 (ST_CP) */
-#define SH_CP_PIN PC2       /* Shift clock at pin PC2 (SH_CP) */
-
-// Define Shift Register actions
-#define DS_low()  LEDS_BAR_PORT&=~(1<<DS_PIN)
-#define DS_high() LEDS_BAR_PORT|=(1<<DS_PIN)
-#define ST_CP_low()  LEDS_BAR_PORT&=~(1<<ST_CP_PIN)
-#define ST_CP_high() LEDS_BAR_PORT|=(1<<ST_CP_PIN)
-#define SH_CP_low()  LEDS_BAR_PORT&=~(1<<SH_CP_PIN)
-#define SH_CP_high() LEDS_BAR_PORT|=(1<<SH_CP_PIN)
 
 //Define functions
 //======================
@@ -21,17 +11,17 @@ void hardware_init(void);
 
 int main (void) {
   hardware_init(); //Setup IO pins and defaults
-
   while(1) {
-    for(int i=0; i<9; i++){
-      leds_bar_output(i);
+    for(int i=0; i<256; i++){
+      shift_register_output(i);
+      _delay_ms(500);
     }
   }
 }
 
 
 void hardware_init (void) {
-
-  leds_bar_init();
+  shift_register_init();
+  uart_init();
   LEDS_BAR_PORT = 0x00; // Set all output pins as low
 }
