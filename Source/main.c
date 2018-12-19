47d0885e69dfc01be4f0dd2695932040ba132a7e
Cássio Santos <sss.cassio@gmail.com>
2018-12-19 00:31:10 +0000

    Add level sensors to main

--- Source/main.c
+++ Source/main.c
@@ -6,6 +6,7 @@
 #include "../Headers/buzzer.h"
 #include "../Headers/sonar.h"
 #include "../Headers/motor.h"
+#include "../Headers/level_sensors.h"
 
 /* Define Functions*/
 void hardware_init(void);
@@ -14,13 +15,16 @@ int main(void)
 {
   hardware_init(); //Setup IO pins and defaults
 
-  uint16_t tank_percent;
+  uint16_t tank_percent, tank_percent_floater, tank_percent_pins, tank_percent_sonar;
+
   while (1)
   {
-    tank_percent = get_floater_percent();
+    tank_percent_floater = get_floater_percent();
+    tank_percent_sonar = get_sonar_percent();
+    tank_percent_pins = get_pins_percent();
+    tank_percent = tank_percent_pins;
     show_displays(99 - tank_percent);
     buzzer_dispatcher(99 - tank_percent);
-    printf("%d\n", get_sonar_value());
     motor_dispatcher(99 - tank_percent);
   }
 }
@@ -33,4 +37,5 @@ void hardware_init(void)
   buzzer_init();
   sonar_init();
   motor_init();
+  level_sensors_init();
 }
